b547b3d5d292a3984c7e021c935e4868346d8b2b
lmitchell524 <lmitchell524@gmail.com>
2018-01-11 17:03:17 -0800

    completed add item with dummy data

---
+++ src/components/list.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+
+class List extends Component{
+    render(){
+
+        const listItems = this.props.list.map((item, index) => {
+            return <li key={index} className="list-group-item">{item.title}</li>
+        })
+
+        return(
+            <div>
+                <div className="row my-4 justify-content-end">
+                    <div className="col-2">
+                        <Link to="/add-item" className="btn btn-outline-primary">Add To Do Item</Link>
+                    </div>
+                </div>
+                <h1 className="text-center">To do List</h1>
+                <ul className="list-group">
+                    {listItems}
+                </ul>
+            </div>
+        )
+    }
+}
+
+function mapStateToProps(state){
+    return {
+        list: state.todo.list
+    }
+}
+
+export default connect(mapStateToProps)(List);
